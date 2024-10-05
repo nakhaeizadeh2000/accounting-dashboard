77aed634ade103305d3c5d7614c1aa0495f24a17
mr-hulk81 <mrhulk20001@gmail.com>
2024-10-05 18:22:55 +0000

    client -> fix -> style underline and li element

--- client/app/(defaults)/user/layout.tsx
+++ client/app/(defaults)/user/layout.tsx
@@ -6,7 +6,7 @@ import { motion } from 'framer-motion';
 import { Route } from 'next';
 import { btnNavigation } from '@/shared/types/btn-navigation.model';
 import { FaUser, FaUserCheck } from 'react-icons/fa6';
-import styles from '../../../components/modules/tab-navigation/style/tab-navigaiton.module.scss';
+import styles from './tab-navigaiton.module.scss';
 import { usePathname } from 'next/navigation';
 
 type Props = {
@@ -19,29 +19,34 @@ const tabs: btnNavigation[] = [
 ];
 
 const Userlayout = (props: Props) => {
+  console.log(styles, 'style');
   const pathName = usePathname();
   console.log(pathName);
   return (
     <>
-      <nav className="flex h-[44px] w-fit items-center rounded-[10px] rounded-b-none border-b border-b-[#eeeeee] bg-[#fdfdfd] p-[5px_5px_0]">
-        <ul className="m-0 flex w-fit list-none p-0 font-['Poppins'] text-[14px] font-medium">
+      <nav className="relative right-2 flex h-[44px] w-fit items-center rounded-lg rounded-b-none border-b border-b-[#eeeeee] bg-[#fdfdfd]">
+        <ul className="m-0 flex h-[100%] w-fit list-none p-0 font-['Poppins'] text-[14px] font-medium">
           {tabs.map(({ Icon, ...item }) => (
-            <Link
-              href={item?.link as Route}
+            <motion.li
               key={item.label}
-              className={`flex h-9 w-40 items-center justify-center rounded-t-lg text-slate-900 ${item.link === pathName ? 'bg-[#eee] text-lg text-secondary' : ''}`}
-              // onClick={() => setSelectedTab(item)}
+              className={`h-[100%] rounded-t-lg ${item.link === pathName ? 'bg-[#eee] text-lg text-secondary' : ''}`}
             >
-              <Icon className="m-2" />
-              {item.label}
-              {item.link === pathName ? (
-                <motion.div className={styles.underline} layoutId="underline" />
-              ) : null}
-            </Link>
+              <Link
+                href={item?.link as Route}
+                className={`flex h-[100%] w-40 items-center justify-center text-slate-900`}
+                // onClick={() => setSelectedTab(item)}
+              >
+                <Icon className="m-2" />
+                {item.label}
+              </Link>
+              {item.link === pathName && (
+                <motion.div className="absolute h-[1px] w-40 bg-[#8855ff]" layoutId="underline" />
+              )}
+            </motion.li>
           ))}
         </ul>
       </nav>
-      <main className="flex h-[500px] grow select-none items-center justify-center rounded-b-lg border-b border-b-[#eeeeee] bg-[#fdfdfd] p-[5px_5px_0]">
+      <main className="flex h-[500px] grow select-none items-center justify-center rounded-sm border-b border-b-[#eeeeee] bg-[#fdfdfd] p-[5px_5px_0]">
         {props.children}
       </main>
     </>
