8abc88cac3597093bcc4988460095f36b628677e
mr-hulk81 <mrhulk20001@gmail.com>
2024-10-06 02:58:28 +0000

    client -> fix -> fix style and logic and create the new component

--- client/app/(defaults)/user/layout.tsx
+++ client/app/(defaults)/user/layout.tsx
@@ -6,11 +6,35 @@ import { motion } from 'framer-motion';
 import { Route } from 'next';
 import { btnNavigation } from '@/shared/types/btn-navigation.model';
 import { FaUser, FaUserCheck } from 'react-icons/fa6';
-import styles from './tab-navigaiton.module.scss';
+// import styles from './tab-navigaiton.module.scss';
 import { usePathname } from 'next/navigation';
 
 type Props = {
   children: React.ReactNode;
+};
+
+const tabVariants = {
+  active: {
+    backgroundColor: '#eee',
+    color: '#8855ff',
+    transition: { duration: 0.3 },
+  },
+  inactive: {
+    backgroundColor: 'transparent',
+    color: '#1e293b',
+    transition: { duration: 0.3 },
+  },
+};
+
+const underlineVariants = {
+  active: {
+    width: '100%',
+    transition: { duration: 0.3 },
+  },
+  inactive: {
+    width: '0%',
+    transition: { duration: 0.3 },
+  },
 };
 
 const tabs: btnNavigation[] = [
@@ -19,7 +43,6 @@ const tabs: btnNavigation[] = [
 ];
 
 const Userlayout = (props: Props) => {
-  console.log(styles, 'style');
   const pathName = usePathname();
   console.log(pathName);
   return (
@@ -29,18 +52,25 @@ const Userlayout = (props: Props) => {
           {tabs.map(({ Icon, ...item }) => (
             <motion.li
               key={item.label}
-              className={`h-[100%] rounded-t-lg ${item.link === pathName ? 'bg-[#eee] text-lg text-secondary' : ''}`}
+              variants={tabVariants}
+              animate={item.link === pathName ? 'active' : 'inactive'}
+              className={`relative h-[100%] rounded-t-lg`}
+              whileHover={item?.link !== pathName ? { scale: 0.9 } : { scale: 1 }}
             >
               <Link
                 href={item?.link as Route}
-                className={`flex h-[100%] w-40 items-center justify-center text-slate-900`}
-                // onClick={() => setSelectedTab(item)}
+                className={`flex h-[100%] w-40 items-center justify-center`}
               >
                 <Icon className="m-2" />
                 {item.label}
               </Link>
               {item.link === pathName && (
-                <motion.div className="absolute h-[1px] w-40 bg-[#8855ff]" layoutId="underline" />
+                <motion.div
+                  className="absolute h-[1px] w-40 bg-[#8855ff]"
+                  layoutId="underline"
+                  variants={underlineVariants}
+                  animate={item.link === pathName ? 'active' : 'inactive'}
+                />
               )}
             </motion.li>
           ))}
