c71fb3ea3056c1c92a34f7305f6f1722ace0e043
mr-hulk81 <mrhulk20001@gmail.com>
2024-10-08 01:32:08 +0000

    client-> add package and fix style -> add mui in project and fix style but have bug in width and scrolling

--- client/app/(defaults)/user/layout.tsx
+++ client/app/(defaults)/user/layout.tsx
@@ -1,11 +1,11 @@
 'use client';
 
 import Link from 'next/link';
-import React, { useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { motion } from 'framer-motion';
 import { Route } from 'next';
 import { btnNavigation } from '@/shared/types/btn-navigation.model';
-import { FaUser, FaUserCheck } from 'react-icons/fa6';
+import { FaChevronLeft, FaChevronRight, FaUser, FaUserCheck } from 'react-icons/fa6';
 // import styles from './tab-navigaiton.module.scss';
 import { usePathname } from 'next/navigation';
 
@@ -40,28 +40,132 @@ const underlineVariants = {
 const tabs: btnNavigation[] = [
   { label: 'لیست', link: '/user/list', Icon: FaUser },
   { label: 'جدید', link: '/user/add', Icon: FaUserCheck },
+  { label: 'جدید', link: '/user/add', Icon: FaUserCheck },
+  { label: 'جدید', link: '/user/add', Icon: FaUserCheck },
+  { label: 'جدید', link: '/user/add', Icon: FaUserCheck },
+  { label: 'جدید', link: '/user/add', Icon: FaUserCheck },
+  { label: 'جدید', link: '/user/add', Icon: FaUserCheck },
+  { label: 'جدید', link: '/user/add', Icon: FaUserCheck },
+  // { label: 'جدید', link: '/user/add', Icon: FaUserCheck },
+  // { label: 'جدید', link: '/user/add', Icon: FaUserCheck },
+  // { label: 'جدید', link: '/user/add', Icon: FaUserCheck },
+  // { label: 'جدید', link: '/user/add', Icon: FaUserCheck },
+  // { label: 'جدید', link: '/user/add', Icon: FaUserCheck },
+  // { label: 'جدید', link: '/user/add', Icon: FaUserCheck },
+  // { label: 'جدید', link: '/user/add', Icon: FaUserCheck },
+  // { label: 'جدید', link: '/user/add', Icon: FaUserCheck },
+  // { label: 'جدید', link: '/user/add', Icon: FaUserCheck },
+  // { label: 'جدید', link: '/user/add', Icon: FaUserCheck },
+  // { label: 'جدید', link: '/user/add', Icon: FaUserCheck },
+  // { label: 'جدید', link: '/user/add', Icon: FaUserCheck },
 ];
 
 const Userlayout = (props: Props) => {
+  // const pathName = usePathname();
+  // const scrollContainerRef = useRef<HTMLUListElement>(null);
+  // const [showLeftFade, setShowLeftFade] = useState(false);
+  // const [showRightFade, setShowRightFade] = useState(true);
+
+  // // const handleScroll = () => {
+  // //   if (scrollContainerRef.current) {
+  // //     const { scrollLeft, scrollWidth, clientWidth } = scrollContainerRef.current;
+  // //     setShowLeftFade(scrollLeft > 0);
+  // //     setShowRightFade(scrollLeft < scrollWidth - clientWidth - 1);
+  // //   }
+  // // };
+
+  // // useEffect(() => {
+  // //   handleScroll();
+  // //   window.addEventListener('resize', handleScroll);
+  // //   return () => window.removeEventListener('resize', handleScroll);
+  // // }, []);
+
+  // // const scroll = (direction: 'left' | 'right') => {
+  // //   if (scrollContainerRef.current) {
+  // //     const scrollAmount = direction === 'left' ? -200 : 200;
+  // //     scrollContainerRef.current.scrollBy({ left: scrollAmount, behavior: 'smooth' });
+  // //   }
+  // // };
+
+  // return (
+  //   <>
+  //     <nav className="relative flex h-[44px] items-center overflow-hidden rounded-lg rounded-b-none border-b border-b-[#eeeeee] bg-[#fdfdfd]">
+  //       {/* {showLeftFade && (
+  //         <div className="absolute left-0 z-10 h-full w-8 bg-gradient-to-r from-white to-transparent">
+  //           <button
+  //             onClick={() => scroll('left')}
+  //             className="h-full w-full text-gray-600 focus:outline-none"
+  //           >
+  //             <FaChevronLeft />
+  //           </button>
+  //         </div>
+  //       )} */}
+  //       <ul
+  //         ref={scrollContainerRef}
+  //         className="scrollbar-hide flex h-full w-full list-none overflow-x-auto p-0 text-[14px] font-medium"
+  //         // onScroll={handleScroll}
+  //       >
+  //         {tabs.map(({ Icon, ...item }) => (
+  //           <motion.li
+  //             key={item.label}
+  //             variants={tabVariants}
+  //             animate={item.link === pathName ? 'active' : 'inactive'}
+  //             className="relative h-full flex-shrink-0"
+  //             whileHover={item?.link !== pathName ? { scale: 0.95 } : { scale: 1 }}
+  //           >
+  //             <Link
+  //               href={item?.link as Route}
+  //               className="flex h-full w-32 items-center justify-center px-2 text-center"
+  //             >
+  //               {Icon && <Icon className="mr-1" />}
+  //               <span className="truncate">{item.label}</span>
+  //             </Link>
+  //             {item.link === pathName && (
+  //               <motion.div
+  //                 className="absolute bottom-0 left-0 h-[2px] w-full bg-[#8855ff]"
+  //                 layoutId="underline"
+  //                 variants={underlineVariants}
+  //                 animate={item.link === pathName ? 'active' : 'inactive'}
+  //               />
+  //             )}
+  //           </motion.li>
+  //         ))}
+  //       </ul>
+  //       {/* {showRightFade && (
+  //         <div className="absolute right-0 z-10 h-full w-8 bg-gradient-to-l from-white to-transparent">
+  //           <button
+  //             onClick={() => scroll('right')}
+  //             className="h-full w-full text-gray-600 focus:outline-none"
+  //           >
+  //             <FaChevronRight />
+  //           </button>
+  //         </div>
+  //       )} */}
+  //     </nav>
+  //     <main className="flex h-[500px] grow select-none items-center justify-center rounded-sm border-b border-b-[#eeeeee] bg-[#fdfdfd] p-[5px_5px_0]">
+  //       {props.children}
+  //     </main>
+  //   </>
+  // );
   const pathName = usePathname();
   console.log(pathName);
   return (
     <>
-      <nav className="relative right-2 flex h-[44px] w-fit items-center rounded-lg rounded-b-none border-b border-b-[#eeeeee] bg-[#fdfdfd]">
-        <ul className="m-0 flex h-[100%] w-fit list-none p-0 font-['Poppins'] text-[14px] font-medium">
+      <nav className="relative flex h-[44px] items-center overflow-hidden rounded-lg rounded-b-none border-b border-b-[#eeeeee] bg-[#fdfdfd] lg:right-2 lg:w-fit">
+        <ul className="scrollbar-hide flex h-full list-none overflow-x-auto p-0 text-[14px] font-medium sm:w-full lg:max-w-[99.5rem]">
           {tabs.map(({ Icon, ...item }) => (
             <motion.li
               key={item.label}
               variants={tabVariants}
               animate={item.link === pathName ? 'active' : 'inactive'}
-              className={`relative h-[100%] rounded-t-lg`}
+              className={`relative h-full flex-shrink-0 rounded-t-lg`}
               whileHover={item?.link !== pathName ? { scale: 0.9 } : { scale: 1 }}
             >
               <Link
                 href={item?.link as Route}
-                className={`flex h-[100%] w-40 items-center justify-center`}
+                className={`flex h-full w-40 items-center justify-center text-center`}
               >
-                <Icon className="m-2" />
+                {Icon && <Icon className="m-2" />}
                 {item.label}
               </Link>
               {item.link === pathName && (
