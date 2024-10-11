0269a4bd3bfdd79f9c36eb781983a3e77e98a775
mr-hulk81 <mrhulk20001@gmail.com>
2024-10-11 17:45:34 +0000

    client -> fix & bug -> comment the new code in my module

--- client/app/(defaults)/user/TabsUser.ts
+++ client/app/(defaults)/user/TabsUser.ts
@@ -1,50 +1,76 @@
-import { BtnNavigation } from '@/components/modules/tab-navigation/btn-navigation.model';
+import { btnNavigation } from '@/components/modules/tab-navigation/btn-navigation.model';
 import { FaUser, FaUserCheck } from 'react-icons/fa6';
 
-export const btnTabs: BtnNavigation[] = [
-  { type: 'link', label: 'لیست', link: '/user/list', Icon: FaUser },
-  { type: 'link', label: '1جدید', link: '/user/add', Icon: FaUserCheck },
-  { type: 'link', label: '2جدید', link: '/user/add', Icon: FaUserCheck, disableCondition: true },
-  { type: 'link', label: '3جدید', link: '/user/add', Icon: FaUserCheck },
-  { type: 'link', label: '4جدید', link: '/user/add', Icon: FaUserCheck },
-  { type: 'link', label: '5جدید', link: '/user/add', Icon: FaUserCheck },
-  { type: 'link', label: '6جدید', link: '/user/add', Icon: FaUserCheck },
-  { type: 'link', label: '7جدید', link: '/user/add', Icon: FaUserCheck },
-  { type: 'link', label: '8جدید', link: '/user/add', Icon: FaUserCheck },
-  { type: 'link', label: '9جدید', link: '/user/add', Icon: FaUserCheck },
-  {
-    type: 'dropDown',
-    label: '10جدید',
-    Icon: FaUserCheck,
-    children: [
-      { type: 'link', label: '1جدید', link: '/user/add', Icon: FaUserCheck },
-      { type: 'link', label: '3جدید', link: '/user/add', Icon: FaUserCheck },
-      { type: 'link', label: '5جدید', link: '/user/add', Icon: FaUserCheck },
-      { type: 'link', label: '7جدید', link: '/user/add', Icon: FaUserCheck },
-    ],
-  },
-  {
-    type: 'dropDown',
-    label: '11جدید',
-    Icon: FaUserCheck,
-    children: [
-      { type: 'link', label: '4جدید', link: '/user/add', Icon: FaUserCheck },
-      { type: 'link', label: '6جدید', link: '/user/add', Icon: FaUserCheck },
-      { type: 'link', label: '8جدید', link: '/user/add', Icon: FaUserCheck },
-      { type: 'link', label: '10جدید', link: '/user/add', Icon: FaUserCheck },
-    ],
-  },
-  { type: 'link', label: '12جدید', link: '/user/add', Icon: FaUserCheck },
-  {
-    type: 'dropDown',
-    label: '13جدید',
-    Icon: FaUserCheck,
-    children: [
-      { type: 'link', label: '11جدید', link: '/user/add', Icon: FaUserCheck },
-      { type: 'link', label: '13جدید', link: '/user/add', Icon: FaUserCheck },
-      { type: 'link', label: '17جدید', link: '/user/add', Icon: FaUserCheck },
-      { type: 'link', label: '19جدید', link: '/user/add', Icon: FaUserCheck },
-    ],
-  },
-  { type: 'link', label: '14جدید', link: '/user/add', Icon: FaUserCheck },
+export const btnTabs: btnNavigation[] = [
+  { label: 'لیست', link: '/user/list', Icon: FaUser },
+  { label: '1جدید', link: '/user/add', Icon: FaUserCheck },
+  { label: '2جدید', link: '/user/add', Icon: FaUserCheck, disableCondition: true },
+  { label: '3جدید', link: '/user/add', Icon: FaUserCheck },
+  { label: '4جدید', link: '/user/add', Icon: FaUserCheck },
+  { label: '5جدید', link: '/user/add', Icon: FaUserCheck },
+  { label: '6جدید', link: '/user/add', Icon: FaUserCheck },
+  { label: '7جدید', link: '/user/add', Icon: FaUserCheck },
+  { label: '8جدید', link: '/user/add', Icon: FaUserCheck },
+  { label: '9جدید', link: '/user/add', Icon: FaUserCheck },
+  { label: '10جدید', link: '/user/add', Icon: FaUserCheck },
+  { label: '11جدید', link: '/user/add', Icon: FaUserCheck },
+  { label: '12جدید', link: '/user/add', Icon: FaUserCheck },
+  { label: '13جدید', link: '/user/add', Icon: FaUserCheck },
+  { label: '14جدید', link: '/user/add', Icon: FaUserCheck },
+  { label: '15جدید', link: '/user/add', Icon: FaUserCheck },
+  { label: '16جدید', link: '/user/add', Icon: FaUserCheck },
+  { label: '17جدید', link: '/user/add', Icon: FaUserCheck },
+  { label: '18جدید', link: '/user/add', Icon: FaUserCheck },
+  { label: '19جدید', link: '/user/add', Icon: FaUserCheck },
 ];
+
+// import { BtnNavigation } from '@/components/modules/tab-navigation/btn-navigation.model';
+// import { FaUser, FaUserCheck } from 'react-icons/fa6';
+
+// export const btnTabs: BtnNavigation[] = [
+//   { type: 'link', label: 'لیست', link: '/user/list', Icon: FaUser },
+//   { type: 'link', label: '1جدید', link: '/user/add', Icon: FaUserCheck },
+//   { type: 'link', label: '2جدید', link: '/user/add', Icon: FaUserCheck, disableCondition: true },
+//   { type: 'link', label: '3جدید', link: '/user/add', Icon: FaUserCheck },
+//   { type: 'link', label: '4جدید', link: '/user/add', Icon: FaUserCheck },
+//   { type: 'link', label: '5جدید', link: '/user/add', Icon: FaUserCheck },
+//   { type: 'link', label: '6جدید', link: '/user/add', Icon: FaUserCheck },
+//   { type: 'link', label: '7جدید', link: '/user/add', Icon: FaUserCheck },
+//   { type: 'link', label: '8جدید', link: '/user/add', Icon: FaUserCheck },
+//   { type: 'link', label: '9جدید', link: '/user/add', Icon: FaUserCheck },
+//   {
+//     type: 'dropDown',
+//     label: '10جدید',
+//     Icon: FaUserCheck,
+//     children: [
+//       { type: 'link', label: '1جدید', link: '/user/add', Icon: FaUserCheck },
+//       { type: 'link', label: '3جدید', link: '/user/add', Icon: FaUserCheck },
+//       { type: 'link', label: '5جدید', link: '/user/add', Icon: FaUserCheck },
+//       { type: 'link', label: '7جدید', link: '/user/add', Icon: FaUserCheck },
+//     ],
+//   },
+//   {
+//     type: 'dropDown',
+//     label: '11جدید',
+//     Icon: FaUserCheck,
+//     children: [
+//       { type: 'link', label: '4جدید', link: '/user/add', Icon: FaUserCheck },
+//       { type: 'link', label: '6جدید', link: '/user/add', Icon: FaUserCheck },
+//       { type: 'link', label: '8جدید', link: '/user/add', Icon: FaUserCheck },
+//       { type: 'link', label: '10جدید', link: '/user/add', Icon: FaUserCheck },
+//     ],
+//   },
+//   { type: 'link', label: '12جدید', link: '/user/add', Icon: FaUserCheck },
+//   {
+//     type: 'dropDown',
+//     label: '13جدید',
+//     Icon: FaUserCheck,
+//     children: [
+//       { type: 'link', label: '11جدید', link: '/user/add', Icon: FaUserCheck },
+//       { type: 'link', label: '13جدید', link: '/user/add', Icon: FaUserCheck },
+//       { type: 'link', label: '17جدید', link: '/user/add', Icon: FaUserCheck },
+//       { type: 'link', label: '19جدید', link: '/user/add', Icon: FaUserCheck },
+//     ],
+//   },
+//   { type: 'link', label: '14جدید', link: '/user/add', Icon: FaUserCheck },
+// ];
--- client/components/modules/tab-navigation/BtnNavigationComponent.tsx
+++ client/components/modules/tab-navigation/BtnNavigationComponent.tsx
@@ -6,10 +6,10 @@ import Tab from '@mui/material/Tab';
 import { Box } from '@mui/material';
 import Link from 'next/link';
 import { Route } from 'next';
-import { BtnNavigation } from './btn-navigation.model';
+import { btnNavigation } from './btn-navigation.model';
 
 type Props = {
-  btn: BtnNavigation[];
+  btn: btnNavigation[];
   children: React.ReactNode;
 };
 
@@ -41,7 +41,7 @@ const BtnNavigationComponent = ({ btn, children }: Props) => {
               key={item?.label}
               label={item?.label}
               component={Link}
-              href={item?.type === 'link' ? (item?.link as Route) : '/'}
+              href={item?.link as Route}
               icon={<Icon />}
               iconPosition="start"
               disabled={item?.disableCondition}
@@ -56,3 +56,58 @@ const BtnNavigationComponent = ({ btn, children }: Props) => {
 };
 
 export default BtnNavigationComponent;
+
+// import React from 'react';
+// import { Tabs, Tab, Menu, MenuItem, Box } from '@mui/material';
+
+// const UsersLayout: React.FC = () => {
+//   const [selectedTab, setSelectedTab] = React.useState<number | false>(0);  // Keeps track of active tab
+//   const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null); // Anchor for dropdown
+
+//   // Handle Tab change except for the "More" Tab
+//   const handleChange = (event: React.SyntheticEvent, newValue: number) => {
+//     if (newValue !== 2) {
+//       setSelectedTab(newValue);  // Change tab only for non-dropdown tabs
+//     }
+//   };
+
+//   // Open dropdown for the "More" tab
+//   const handleClick = (event: React.MouseEvent<HTMLElement>) => {
+//     setAnchorEl(event.currentTarget);
+//     setSelectedTab(false); // Keep selected tab disabled since this is a dropdown
+//   };
+
+//   // Close dropdown
+//   const handleClose = () => {
+//     setAnchorEl(null);
+//   };
+
+//   return (
+//     <Box sx={{ width: '100%' }}>
+//       {/* Tabs with one dropdown */}
+//       <Tabs value={selectedTab} onChange={handleChange} aria-label="User Tabs">
+//         <Tab label="لیست کاربران" />
+//         <Tab label="افزودن کاربر" />
+//         <Tab
+//           label="بیشتر"
+//           onClick={handleClick}  // Trigger dropdown on click
+//         />
+//       </Tabs>
+
+//       {/* Dropdown for the "More" Tab */}
+//       <Menu
+//         anchorEl={anchorEl}
+//         open={Boolean(anchorEl)}
+//         onClose={handleClose}
+//         MenuListProps={{
+//           'aria-labelledby': 'more-tab',
+//         }}
+//       >
+//         <MenuItem onClick={handleClose}>ویرایش کاربر</MenuItem>
+//         <MenuItem onClick={handleClose}>حذف کاربر</MenuItem>
+//       </Menu>
+//     </Box>
+//   );
+// };
+
+// export default UsersLayout;
