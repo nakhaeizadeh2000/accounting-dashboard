9e9f312fada147b8e48ddc59c77180577eaace39
Erfan_2002 <afshar.erfan2001@gmail.com>
2025-04-26 16:05:25 +0330

    client -> Fix -> remove postinstall scripts from package.json and update postinstall.js comments

--- client/postinstall.js
+++ client/postinstall.js
@@ -3,3 +3,11 @@ const path = require('path');
 const topDir = __dirname;
 fse.emptyDirSync(path.join(topDir, 'public', 'tinymce'));
 fse.copySync(path.join(topDir, 'node_modules', 'tinymce'), path.join(topDir, 'public', 'tinymce'), { overwrite: true });
+
+
+//* This script is used to copy the tinymce folder from node_modules to public/tinymce
+//* It is run after the installation of the dependencies
+//* It is used to make sure that the tinymce folder is always up to date
+//* It is run after the installation of the dependencies
+// "postinstall": "node ./postinstall.js",
+// "postinstall:check": "node ./postinstall.js --check"
