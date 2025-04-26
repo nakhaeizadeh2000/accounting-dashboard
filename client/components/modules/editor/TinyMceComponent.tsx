252c71fcd2cd9e3be296d9fb15bd616c5f7de211
mr-hulk81 <mrhulk20001@gmail.com>
2025-04-26 19:11:15 +0000

    client -> Fix -> feat(editor): enhance TinyMCE integration and update postinstall script for better asset management

--- client/components/modules/editor/TinyMceComponent.tsx
+++ client/components/modules/editor/TinyMceComponent.tsx
@@ -8,10 +8,8 @@ import { IRootState } from '@/store';
 import { useDispatch } from 'react-redux';
 import { toggleTheme } from '@/store/features/theme/themeConfigSlice';
 import { InitOptions } from '@tinymce/tinymce-react/lib/cjs/main/ts/components/Editor';
-
-// const Editor = dynamic(() => import('@tinymce/tinymce-react').then((mod) => mod.Editor), {
-//   ssr: false,
-// });
+import dynamic from 'next/dynamic';
+import { yekanBakh } from '@/app/stuff/fonts';
 
 interface EditorProps {
   initialValue?: string;
@@ -28,94 +26,101 @@ const TinyEditor = ({ initialValue = '', onChange, initOptionsTinyMce }: EditorP
 
   useEffect(() => {
     const themeMode = dispatch(toggleTheme(localStorage.getItem('theme') || themeConfig.theme));
-
-    console.log(process.env.PUBLIC_URL);
   }, [dispatch, themeConfig.theme]);
 
   return (
-    <Editor
-      // tinymceScriptSrc="/assets/tinymce/tinymce.min.js"
-      tinymceScriptSrc="/tinymce/tinymce.min.js"
-      key={themeConfig.theme}
-      licenseKey="gpl"
-      // apiKey="no api key" // Get this from https://www.tiny.cloud/
-      onInit={(evt, editor) => (editorRef.current = editor)}
-      initialValue={initialValue}
-      init={{
-        branding: false,
-        statusbar: false,
-        height: 500,
-        menubar: false,
-        plugins: [
-          'advlist',
-          'autolink',
-          'lists',
-          'link',
-          'image',
-          'charmap',
-          'preview',
-          'anchor',
-          'searchreplace',
-          'visualblocks',
-          'code',
-          'fullscreen',
-          'insertdatetime',
-          'media',
-          'table',
-          'code',
-          'help',
-          'wordcount',
-          'emoticons',
-          'save',
-          'autosave',
-          'codesample',
-          'directionality',
-          'visualchars',
-          'nonbreaking',
-          'pagebreak',
-          'quickbars',
-          'importcss',
-        ],
-        skin: themeConfig.theme === 'dark' ? 'oxide-dark' : 'oxide',
-        content_css: themeConfig.theme === 'dark' ? 'tinymce-5-dark' : 'tinymce-5',
-        toolbar:
-          'undo redo | formatselect | ' +
-          'bold italic backcolor |alignright alignleft aligncenter ' +
-          ' alignjustify | bullist numlist outdent indent | ' +
-          'removeformat | help | image media link table | ' +
-          'fullscreen preview | emoticons | save | print',
-        content_style: 'body { font-family:var(--font-yekan-bakh); font-size:14px }',
-        language: 'fa',
-        directionality: 'ltr',
-        mobile: {
-          menubar: true,
-          plugins: ['autosave', 'lists', 'autolink'],
-          toolbar: 'undo redo | formatselect | bold italic | bullist numlist',
-        },
-        setup(editor) {
-          editor.on('init', () => {
-            editor.getDoc().body.style.fontSize = '14px';
-            editor.getDoc().body.style.fontFamily = 'var(--font-yekan-bakh)';
-            editor.execCommand('JustifyRight');
-          });
-          editor.on('keydown', (e) => {
-            if (e.key === 'Tab') {
-              e.preventDefault();
-              const newContent = editor.getContent({ format: 'text' }) + '\t';
-              editor.setContent(newContent);
-            }
-          });
-        },
-        default_align: 'right',
-        ...initOptionsTinyMce,
-      }}
-      onEditorChange={(content) => {
-        if (onChange) {
-          onChange(content);
-        }
-      }}
-    />
+    <>
+      <Editor
+        tinymceScriptSrc="/tinymce/tinymce.min.js"
+        key={themeConfig.theme}
+        licenseKey="gpl"
+        // apiKey="no api key" // Get this from https://www.tiny.cloud/
+        onInit={(evt, editor) => (editorRef.current = editor)}
+        initialValue={initialValue}
+        init={{
+          branding: false,
+          statusbar: false,
+          height: 500,
+          min_height: 200,
+          icons: 'material',
+          ui_mode: 'split',
+          menubar: false,
+          language: 'fa',
+          directionality: 'ltr',
+          plugins: [
+            'advlist',
+            'autolink',
+            'lists',
+            'link',
+            'image',
+            'charmap',
+            'preview',
+            'anchor',
+            'searchreplace',
+            'visualblocks',
+            'code',
+            'fullscreen',
+            'insertdatetime',
+            'media',
+            'table',
+            'code',
+            'help',
+            'wordcount',
+            'emoticons',
+            'save',
+            'autosave',
+            'codesample',
+            'directionality',
+            'visualchars',
+            'nonbreaking',
+            'pagebreak',
+            'quickbars',
+            'importcss',
+          ],
+
+          toolbar:
+            'undo redo | formatselect | ' +
+            'bold italic backcolor |alignright alignleft aligncenter ' +
+            ' alignjustify | bullist numlist outdent indent | ' +
+            'removeformat | help | image media link table | ' +
+            'fullscreen preview | emoticons | save | print',
+
+          skin: themeConfig.theme === 'dark' ? 'oxide-dark' : 'oxide',
+          content_css: themeConfig.theme === 'dark' ? 'tinymce-5-dark' : 'tinymce-5',
+          content_style: `body { font-family:${yekanBakh.variable}; font-size:14px }`,
+
+          mobile: {
+            menubar: true,
+            plugins: ['autosave', 'lists', 'autolink'],
+            toolbar: 'undo redo | formatselect | bold italic | bullist numlist',
+          },
+          setup(editor) {
+            editor.on('init', () => {
+              editor.getDoc().body.style.fontSize = '14px';
+              editor.getDoc().body.style.fontFamily = 'font-yekan-bakh';
+              editor.execCommand('JustifyRight');
+            });
+            editor.on('keydown', (e) => {
+              if (e.key === 'Tab') {
+                e.preventDefault();
+                const newContent = editor.getContent({ format: 'text' }) + '\t';
+                editor.setContent(newContent);
+              }
+            });
+          },
+          default_align: 'right',
+          ...initOptionsTinyMce,
+        }}
+        onEditorChange={(content) => {
+          if (onChange) {
+            onChange(content);
+          }
+        }}
+      />
+    </>
   );
 };
 
-export default TinyEditor;
+export default dynamic(() => Promise.resolve(TinyEditor), {
+  ssr: false,
+});
